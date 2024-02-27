684232621441be877082079eb60e8b70b4ee9e86
Barry Bakunowicz <bbach7139@gmail.com>
2024-02-27 12:09:36 -0600

    Create Setup screen + initial flow

--- src/screens/Setup.tsx
+++ src/screens/Setup.tsx
@@ -1,7 +1,30 @@
+import { useTheme } from '@/components/providers/ThemeProvider';
+import ThemeStep from '@/components/setup/ThemeStep';
+import WelcomeStep from '@/components/setup/WelcomeStep';
+import { Config } from '@/types/config';
+import { useState } from 'react';
+
 export default function Setup() {
+  const { theme } = useTheme()
+
+  const initialConfig : Config = {
+    theme: (theme === 'system' ? (window.matchMedia("(prefers-color-scheme: dark)").matches? "dark": "light") : theme),
+    accentColor: `bg-emerald-500`,
+    libraries: []
+  }
+
+  const [config, setConfig] = useState<Config>(initialConfig);
+  const [step, setStep] = useState(0);
+
   return (
-    <>
-      <div className="p-2">Hello from Setup!</div>
-    </>
+    <div className={`m-auto flex flex-col text-center`}>
+      <img className={`w-40 h-40 mx-auto my-auto mb-6`} src='/tauri.svg' alt='logo' />
+      {step === 0 && (
+        <WelcomeStep setStep={setStep}/>
+      )}
+      { step === 1 && (
+        <ThemeStep config={config} setConfig={setConfig} setStep={setStep}/>
+      )}
+    </div>
   )
 }
