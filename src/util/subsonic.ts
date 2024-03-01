8f013eccc99c443c50b644618f16c14b2fe66ea6
Barry Bakunowicz <bbach7139@gmail.com>
2024-02-29 18:21:14 -0600

    Default view to frequent albums instead

--- src/util/subsonic.ts
+++ src/util/subsonic.ts
@@ -32,7 +32,7 @@ export async function getAlbumList(libraries: Library[]) : Promise<Album[]> {
     let offset = 0
 
     let host = library.connectionDetails.host + (library.connectionDetails.port ? `:${library.connectionDetails.port}` : '');
-    let connectionString = `${host}/rest/getAlbumList2.view?type=newest&size=500&offset=${offset}&u=${library.connectionDetails.username}&t=${library.connectionDetails.md5}&s=${library.connectionDetails.salt}&v=1.16.1&c=tauri&f=json`;
+    let connectionString = `${host}/rest/getAlbumList2.view?type=frequent&size=500&offset=${offset}&u=${library.connectionDetails.username}&t=${library.connectionDetails.md5}&s=${library.connectionDetails.salt}&v=1.16.1&c=tauri&f=json`;
 
     console.log('Connection String: ', connectionString);
 
